2292ccf9ea195415732b484ef217bd2b4db9cae7
Daniel Fields <dgfields4193@gmail.com>
2022-12-11 22:16:40 -0500

    WIP: added to game functionality

--- js/script.js
+++ js/script.js
@@ -2,20 +2,24 @@
 //function for stand button thtat shows dealer card and value
 //function for bet
 //function to start game
-//
+// can set money equal to variable then update that and add to html element
+//need to add dealer and player value tracker to appendCard function
 const $dealerCard = $('#dCardOne');
 let $dealCardTwo = $('#dCardTwo')
 let $yourCardOne = $('#youCardOne');
-let $yourCardTwo = $('#youCardTwo');
+const $yourCardTwo = $('#youCardTwo');
 const $pEl = $('#you') 
 //used to track player card value
 let playerValue;
 //used to track dealers card value.
 let dealerValue;
-//sets deck id
+//used to set deck id
 let deckId;
-
+//used to assign cards at start of game
 let startingCards;
+//used to assign card when play clicks hit
+let addCard;
+
 
 //used to retreive deck id for use in start game function
 function getDeckId() {
@@ -31,6 +35,7 @@ function getDeckId() {
         }
     )
 }
+//call getDeckId to allow startGame to have access to deckId
 getDeckId();
 $('#start').on('click', startGame)
 //starts game and draws first four cards
@@ -47,31 +52,37 @@ function startGame() {
         }
     )
 }
-
+//sets initial card images and count for game
 function initialCard() {
-    $dealerCard.attr('src',`${startingCards.cards[0].image}`)
-    $dealCardTwo.attr('src', `${startingCards.cards[1].image}`)
-    $yourCardOne.attr('src', `${startingCards.cards[2].image}`)
-    $yourCardTwo.attr('src', `${startingCards.cards[3].image}`)
+    $dealerCard.attr('src',`${startingCards.cards[0].image}`);
+    $dealCardTwo.attr('src', `${startingCards.cards[1].image}`);
+    $yourCardOne.attr('src', `${startingCards.cards[2].image}`);
+    $yourCardTwo.attr('src', `${startingCards.cards[3].image}`);
+    dealerValue = Number(startingCards.cards[0].value) + Number(startingCards.cards[1].value);
+    $('#dealerCount').text(dealerValue);
+    playerValue = Number(startingCards.cards[2].value) + Number(startingCards.cards[3].value);
+    $('#playerCount').text(playerValue)
 }
 
 $('#hit').on('click', drawCard)
 
 function drawCard(){
-    appendCard()
-    console.log(appendCard)
     $.ajax({
         url: "https://deckofcardsapi.com/api/deck/"+deckId+"/draw/?count=1"
     }).then(
         (data) => {
-            console.log(data)
+            addCard = data
+            appendCard()
         },
         (error) => {
             console.log('bad request')
         }
     )
 }
+
 function appendCard(){
     const $imgEl = $(`<img>`)
-    $('#you').append($imgEl)
+    $imgEl.insertAfter($yourCardTwo)
+    $imgEl.attr('src', `${addCard.cards[0].image}`)
+    //need to add dealer and player value tracker here
 }
